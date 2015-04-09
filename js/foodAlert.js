f38f84e718fe0a78eace1420c90b4a14da34c196
Joshin <JoshuaShin96@gmail.com>
2015-04-09 12:39:08 -0400

    added iron router implementation fo rreceiving sms

--- js/foodAlert.js
+++ js/foodAlert.js
@@ -1,9 +1,7 @@
 Menus = new Mongo.Collection("Menus");
 Records = new Mongo.Collection("Records");
 
- Router.map(function(){
-  this.route('test', {path: '/cool'});
- });
+
 
 if (Meteor.isClient) {
 	Schemas = {};
@@ -151,6 +149,38 @@ if (Meteor.isClient) {
 
 
 if (Meteor.isServer) {
+
+	Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
+        extended: false
+    }));
+
+  	Router.route('/response/', function () {
+  		var text = this.request.body.Body;
+  		var phone = this.request.body.From;
+  		sendSMS(phone, text);
+  	},
+  	{where: 'server'});
+
+
+	var sendSMS = function (number, text) {
+		Meteor.http.post('https://api.twilio.com/2010-04-01/Accounts/AC22ef9acc63bf954b3e9fdff5762f0bfc/SMS/Messages.json',
+		{
+			params:{From:'+16098794415', To: number, Body: text},
+			auth: 'AC22ef9acc63bf954b3e9fdff5762f0bfc:5eca12596eaa0ccb2e73d1aa6aa419c0',
+			headers: {'content-type':'application/x-www-form-urlencoded'}
+		}, function () {
+			console.log(arguments)
+		});
+	};
+
+
+
+	// Router.route('/response/:_id', {
+	// 	data: function() {
+	// 		console.log('hi');
+	// 	}
+	// },
+	// {where: 'server'});
 
 	/********** UPDATING MENUS DAILY *******/
 	var getMenus = function () {
